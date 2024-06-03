3fcd4cc55e12813d30f6ad4bb6d1f7aef9819e02
Siddharth <st22041@ormiston.school.nz>
2024-06-03 18:43:58 +1200

    Buttons! YAY!

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from src.buttons import Button
 pygame.init()
 
 SCREEN = pygame.display.set_mode((0,0))
+WINDOWSIZE = pygame.display.get_window_size()
 
+main_menu_button = Button([WINDOWSIZE[0] // 2, 100], "Press spacebar to start", font_size=36)
+main_menu_button.set_colour((255,10,10), 255)
+main_menu_button.set_pos([WINDOWSIZE[0] // 2, WINDOWSIZE[1] // 2])
+
+clock = pygame.time.Clock()
+
+started = False
 while True:
+    clock.tick(60)
     keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -16,5 +26,14 @@ while True:
     if keys[pygame.K_q]:
         pygame.quit()
         sys.exit()
-    
-    Button.onclick((200,200), (400,400))
+
+    SCREEN.fill((0,0,0))
+
+    if started == False:
+        main_menu_button.display(SCREEN)
+        pygame.display.flip()
+        if keys[pygame.K_SPACE]:
+            started = True
+        continue
+
+    pygame.display.flip()
--- src/buttons.py
+++ src/buttons.py
@@ -1,17 +1,36 @@
 import pygame
 
 class Button:
-    @staticmethod
-    def onclick(coords0, coords1):
+    build_menu_buttons = []
+    main_menu_buttons = []
+
+    def __init__(self, dimensions: list[int, int], text: str, font_type = pygame.font.get_default_font(), font_size = 18, font_colour = (255,255,255)) -> None:
+        self.dimensions = dimensions
+        self.pos = [0,0]
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], self.dimensions[0], self.dimensions[1])
+        self.colour = pygame.color.Color(255,255,255,255)
+        self.text = text
+        self.font_colour = font_colour
+        self.font_size = font_size
+        self.font_type = font_type
+        self.font = pygame.font.Font(self.font_type, self.font_size)
+        self.rendered_text = self.font.render(self.text, True, self.font_colour)
+    
+    def set_colour(self, colour: tuple, alpha=255):
+        self.colour.update(colour[0], colour[1], colour[2], alpha)
+
+    def set_pos(self, pos: list[int, int]):
+        self.rect.center = pos
+        self.text_rect = self.rendered_text.get_rect(center=pos)
+
+    def pressed(self):
         mouse_pos = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
             print("Pressed")
-            if mouse_pos[0] <= coords1[0] and mouse_pos[0] >= coords0[0]:
-                if mouse_pos[1] >= coords0[1] and mouse_pos[1] <= coords1[1]:
+            if mouse_pos[0] <= self.dimensions[0] and mouse_pos[0] >= self.pos[0]:
+                if mouse_pos[1] >= self.pos[1] and mouse_pos[1] <= self.dimensions[1]:
                     return True
     
-    def hide():
-        pass
-
-    def show():
-        pass
+    def display(self, screen: pygame.Surface):
+        pygame.draw.rect(screen, self.colour, self.rect)
+        screen.blit(self.rendered_text, self.text_rect)
